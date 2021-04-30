f4bc00e191d6378b157bf8b25cfe2c8910caebc0
Joel <joeldse16@gmail.com>
2021-04-29 21:27:45 -0300

    adição de javascript

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var nome = "Joel S. Evangelista"
+var ano = "2021"
+var linkedin = "@joeldse"
+var sexo = "Masculino"
+var polo = "Alphaville"
+
+document.write (nome,"<br/>")
+document.write ("Ano - ",ano,"<br/>")
+document.write ("LinkedIn: ",linkedin,"<br/>")
+document.write ("Sexo: ",sexo,"<br/>")
+document.write ("Localização - polo: ",polo,"<br/>")
+ 
